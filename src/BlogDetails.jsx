fe4207cace2f6c1c990f2bb1de9c17d544b3c73d
AmineBaray <barayamine101@gmail.com>
2024-08-04 02:41:02 +0100

    Added POST request to submit new blog data to the server with fetch API

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -2,17 +2,17 @@ import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 function BlogDetails() {
     const { id } = useParams();
-    const {data: blogs, Pending, error} = useFetch("http://localhost:8000/blogs/" + id);
+    const {data: blog, Pending, error} = useFetch("http://localhost:8000/blogs/" + id);
   return (
     <>
         <div className="ml-64 pt-20 w-2/3">
             {error && <div className='flex items-center justify-center text-3xl text-red-600 mt-8'>{error}</div>}
             {Pending && <div className='flex items-center justify-center text-3xl mt-8'>Loading....</div>}
-            {blogs && (
+            {blog && (
                 <article>
-                    <h2 className='text-7xl font-bold text-sky-700'>{blogs.title}</h2>
-                    <p className='p-2 text-xl'>written by <u className="text-red-500">{blogs.author}</u></p>
-                    <p className='p-2 text-xl'>{blogs.body}</p>
+                    <h2 className='text-7xl font-bold text-sky-700'>{blog.title}</h2>
+                    <p className='p-2 text-xl'>written by <u className="text-red-500">{blog.author}</u></p>
+                    <p className='p-2 text-xl'>{blog.body}</p>
                 </article> 
             )}
         </div>
--- src/NewBlog.jsx
+++ src/NewBlog.jsx
@@ -3,37 +3,64 @@ function NewBlog () {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const [body, setBody] = useState('');
+    const [isPending, setIsPending] = useState(false);
+    const handleSubmit = (e) => {
+        // preventDefault stop the page reload because the form by default reload!
+        e.preventDefault();
+        const blog = {title, author, body};
+        setIsPending(true);
+        fetch("http://localhost:8000/blogs" , {
+            method: 'POST',
+            headers: {"Content-type":"application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log("the blog is added");
+            setIsPending(false);
+        })
+    }
     return ( 
         <>
             <div className="ml-64 pt-20 w-2/3">
                 <h2 className="text-4xl font-medium p-5">Add a New Blog</h2>
-                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2">Blog title:</label>
+                <form 
+                onSubmit={handleSubmit}
+                className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <label className="block text-gray-700 text-xl font-bold m-5">Blog title:</label>
                     <input
                      type="text"
                      required
                      value={title}
                      onChange={(e) => setTitle(e.target.value)}
-                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                      focus:outline-none focus:shadow-outline"
                     />
-                    <label className="block text-gray-700 text-sm font-bold mb-2">Blog author:</label>
+                    <label className="block text-gray-700 text-xl font-bold m-5">Blog author:</label>
                     <input
                      type="text"
                      required
                      value={author}
                      onChange={(e) => setAuthor(e.target.value)}
-                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                      focus:outline-none focus:shadow-outline"
                     />
-                    <label>Blog body:</label>
+                    <label className='block text-gray-700 text-xl font-bold m-5'>Blog body:</label>
                     <textarea 
                     required
                     value={body}
                      onChange={(e) => setBody(e.target.value)}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
-                    <button className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add blog</button>
-                <p>{title}</p>
-                <p>{author}</p>
-                <p>{body}</p>
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                     focus:outline-none focus:shadow-outline"></textarea>
+                    
+                    { !isPending && <button 
+                    className="m-4 text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none
+                     focus:ring-sky-300 font-medium rounded-lg text-xl px-5 py-2.5 text-center dark:bg-sky-600
+                      dark:hover:bg-sky-700 dark:focus:ring-sky-800">
+                        Add blog</button>}
+                    { isPending && <button disabled
+                    className="m-4 text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none
+                     focus:ring-sky-300 font-medium rounded-lg text-xl px-5 py-2.5 text-center dark:bg-sky-600
+                      dark:hover:bg-sky-700 dark:focus:ring-sky-800" >
+                        Adding blog.....</button>}
                 </form>
             </div>
             
