713b2a83242c0044dd56028af35042ab6def943a
AmineBaray <barayamine101@gmail.com>
2024-08-04 01:39:00 +0100

    Added form for creating a new blog in NewBlog component

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 function BlogDetails() {
     const { id } = useParams();
+    const {data: blogs, Pending, error} = useFetch("http://localhost:8000/blogs/" + id);
   return (
     <>
         <div className="ml-64 pt-20 w-2/3">
-            <h1 className="text-xl font-semibold">Blog Details - { id }</h1>
+            {error && <div className='flex items-center justify-center text-3xl text-red-600 mt-8'>{error}</div>}
+            {Pending && <div className='flex items-center justify-center text-3xl mt-8'>Loading....</div>}
+            {blogs && (
+                <article>
+                    <h2 className='text-7xl font-bold text-sky-700'>{blogs.title}</h2>
+                    <p className='p-2 text-xl'>written by <u className="text-red-500">{blogs.author}</u></p>
+                    <p className='p-2 text-xl'>{blogs.body}</p>
+                </article> 
+            )}
         </div>
     </>
   );
--- src/NewBlog.jsx
+++ src/NewBlog.jsx
@@ -1,14 +1,40 @@
-import Navbar from "./Navbar"
-
+import {useState} from 'react'
 function NewBlog () {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [body, setBody] = useState('');
     return ( 
         <>
             <div className="ml-64 pt-20 w-2/3">
-                <p className="text-xl font-semibold">
-                    Lorem ipsum dolor sit amet consectetur,
-                    adipisicing elit. Harum omnis, saepe eum
-                    dolorem enim adipisci consequuntur molestias.
-                </p>
+                <h2 className="text-4xl font-medium p-5">Add a New Blog</h2>
+                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Blog title:</label>
+                    <input
+                     type="text"
+                     required
+                     value={title}
+                     onChange={(e) => setTitle(e.target.value)}
+                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Blog author:</label>
+                    <input
+                     type="text"
+                     required
+                     value={author}
+                     onChange={(e) => setAuthor(e.target.value)}
+                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                    <label>Blog body:</label>
+                    <textarea 
+                    required
+                    value={body}
+                     onChange={(e) => setBody(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+                    <button className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add blog</button>
+                <p>{title}</p>
+                <p>{author}</p>
+                <p>{body}</p>
+                </form>
             </div>
             
         </>
